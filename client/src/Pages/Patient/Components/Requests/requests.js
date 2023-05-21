88fcd1f76669897874d0b3e48f3af2ea8b8eddf1
Helmirmili3 <helmirmili3@gmail.com>
2023-05-21 15:33:50 +0200

    This is the 7.0.5

--- client/src/Pages/Patient/Components/Requests/requests.js
+++ client/src/Pages/Patient/Components/Requests/requests.js
@@ -6,30 +6,39 @@ import WaitingRequests from "./WaitingRequests/waitingRequests";
 import { usePatient } from "../../../../Contexts/patientContext";
 const PatientRequests = () => {
   const { requests } = usePatient();
+  console.log(requests)
   const list = requests?.slice().reverse();
   const oldRequest = list.filter((request) => {
+    if (request.isSeenBySeller) {
+      return true;
+    }
+    return false;
+  });
+
+  const waitingRequestsList = list.filter((_request) => {
     if (
-      (request.isSeenBySeller && !request.isAcceptedBySeller) ||
-      (request.isSeenBySeller &&
-        request.isAcceptedBySeller &&
-        request.isSeenByBuyer)
+      _request.isSeenBySeller == false 
     ) {
       return true;
     }
     return false;
   });
-  const sendedRequests = list.filter((request) => !request.isSeenBySeller);
-  const waitingRequests = list.filter((request) => {
-    if (request.isAcceptedBySeller && !request.isPayedByBuyer) {
-      return true;
-    }
-    return false;
-  });
+  // const rejectedPayment = list.filter((request) => {
+  //   if (
+  //     request.isPayedByBuyer == true &&
+  //     request.isSeenBySeller == true &&
+
+  //   ) {
+  //     return true;
+  //   }
+  //   return false;
+  // });
+
   return list ? (
     <>
-      <WaitingRequests requests={waitingRequests} />
-      <RejectedRequests requests={sendedRequests} />
-      <HistoryRequests requests={oldRequest} />
+      <WaitingRequests requests={waitingRequestsList} />
+      {/* <RejectedRequests requests={rejectedPayment} />
+      <HistoryRequests requests={oldRequest} /> */}
     </>
   ) : (
     <></>
